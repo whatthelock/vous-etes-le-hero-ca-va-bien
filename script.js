6bbfd50de9d790087a1a9315b216e05471b5b0ef
Loïc Delorme <locdelorme@gmail.com>
2021-09-29 10:19:24 -0400

    base js

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstchapter = {
+  subtitle: "firstchapter",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+  img: "",
+};
+
+chapterObj = {
+  slug: "",
+};
+
+let firstchapter = {
+  subtitle: "firstchapter",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+  img: "",
+};
+
+chapterObj = {
+  slug: "",
+};
