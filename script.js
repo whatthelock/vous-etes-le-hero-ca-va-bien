cb3025d76ccdf121fe2e29b4e730cc0ee155a56f
Loïc Delorme <locdelorme@gmail.com>
2021-09-30 02:53:11 -0400

    fin 1 projet 30 sept

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const firstchapter = {
 const journeMaison = {
   subtitle: "Ta journée à la maison",
   text: "Maintenant que tu ne va plus a l'école, tu dois trouver quelque chose a manger. Tu va regarder dans ton frigo...OH MALHEUR!!! I ne reste plus rine à manger dans ta maison!!!! QUe va tu faire!?",
-  img: "",
+  img: "frigo.jpg",
 
   options: [
     (commender = {
@@ -35,7 +35,7 @@ const journeMaison = {
 const choix_bouffe = {
   subtitle: "Aventure à l'épicerie",
   text: "Tu viens tout juste de renter à l'épicerie, malheureusement du a la pandémie, la majorité des items ont déja été venduent!! De plus tu réalise que tu as oublié ton porte-feuille à la maison.... Tu dois donc te limiter a un items et choisir dans l'inventaire très renstraint:",
-  img: "",
+  img: "epicerie.jpg",
 
   options: [
     (bananes = {
@@ -50,10 +50,9 @@ const choix_bouffe = {
 };
 
 const arivéeÉcole = {
-  subtitle:
-    "Tu viens d'arriver à l'école, de manière a faire attention a la pandémie tu dois choisir ton occupation judicieusement; tu peux aller dans ta petite classe fermer pour assister a ton cours, tu peux aller vers la cafétéria ou tu pourra travailler sur tes devoirs et avoir plus d'espace ou encore aller dehors...",
-  text: "",
-  img: "",
+  subtitle: "Arrivée à l'école",
+  text: "Tu viens d'arriver à l'école, de manière a faire attention a la pandémie tu dois choisir ton occupation judicieusement; tu peux aller dans ta petite classe fermer pour assister a ton cours, tu peux aller vers la cafétéria ou tu pourra travailler sur tes devoirs et avoir plus d'espace ou encore aller dehors...",
+  img: "ecole.jpg",
 
   options: [
     (classe = {
@@ -74,7 +73,34 @@ const arivéeÉcole = {
 const vaccin = {
   subtitle: "Offre vaccinale",
   text: "En arrivant à la cafétéria, tu croise un clinique de vaccination qui t'offre un vaccin sur place. Le vaccin semble légérement louche, l'accepte tu malgré ta phobie des aiguilles??",
-  img: "",
+  img: "vaccin.jpg",
+
+  options: [
+    (classe = {
+      text: "aller en classe",
+      action: "goToChapter('retour_maison')",
+    }),
+    (cafet = {
+      text: "Aller a la cafétéria",
+      action: "goToChapter('offre_vaccinale')",
+    }),
+    (dehors = {
+      text: "Aller dehors",
+      action: "goToChapter('you_die')",
+    }),
+  ],
+};
+
+const vaccin = {
+  subtitle: "Offre vaccinale",
+  text: "En arrivant à la cafétéria, tu croise un clinique de vaccination qui t'offre un vaccin sur place. Le vaccin semble légérement louche, l'accepte tu malgré ta phobie des aiguilles??",
+  img: "vaccin.jpg",
+};
+
+const mort = {
+  subtitle: "VOUS ETES MORT",
+  text: "Vous avez atttrapé la maladie, après de long jour à l'hopital à vous accrocher à la vie entouré de votre famille cher, la pandemie a eu raion de vous, reposer en paix.",
+  img: "rip.jpg",
 };
 
 const chapterObj = {
@@ -82,6 +108,8 @@ const chapterObj = {
   journee_a_maison: journeMaison,
   aventure_epicerie: choix_bouffe,
   offre_vaccinale: vaccin,
+  annonce_pandemie: firstchapter,
+  you_die: mort,
 };
 
 function goToChapter(chapterName) {
