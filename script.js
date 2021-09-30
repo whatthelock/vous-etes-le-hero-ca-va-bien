fd75e629428b00724a8bf152373e0a4d0409e31f
Loïc Delorme <locdelorme@gmail.com>
2021-09-30 10:34:34 -0400

    fin-3

--- script.js
+++ script.js
@@ -1,121 +1,113 @@
 /*Tu te reveille un doux matin de printemps, le soleil illumine doucement ta chambre avec de beaux faissauts lumineux. */
-const firstchapter = {
-  subtitle: "La pandémie annoncée",
-  text: "En descandant pour dejeuner tu apprends une grave nouvelle: UNE PANDÉMIE MONDIALE VIENT D'ÊTRE ANNONCÉ TU DOIS TOUT FAIRE EN TON POUVOIR POUR TE PROTEGER TOI ET TA FAMILLE",
-  img: "pandemie.jpg",
-  options: [
-    (maison = {
-      text: "Sécher les cours par protection",
-      action: "goToChapter('journee_a_maison')",
-    }),
-    (ecole = {
-      text: "Aller à l'école quand même",
-      action: "goToChapter('arivee_ecole')",
-    }),
-  ],
-};
-
-const journeMaison = {
-  subtitle: "Ta journée à la maison",
-  text: "Maintenant que tu ne va plus a l'école, tu dois trouver quelque chose a manger. Tu va regarder dans ton frigo...OH MALHEUR!!! I ne reste plus rine à manger dans ta maison!!!! QUe va tu faire!?",
-  img: "frigo.jpg",
-
-  options: [
-    (commender = {
-      text: "commender de la nourriture",
-      action: "goToChapter('you_die')",
-    }),
-    (epicerie = {
-      text: "Aller à l'épicerie",
-      action: "goToChapter('aventure_epicerie')",
-    }),
-  ],
-};
-
-const choix_bouffe = {
-  subtitle: "Aventure à l'épicerie",
-  text: "Tu viens tout juste de renter à l'épicerie, malheureusement du a la pandémie, la majorité des items ont déja été venduent!! De plus tu réalise que tu as oublié ton porte-feuille à la maison.... Tu dois donc te limiter a un items et choisir dans l'inventaire très renstraint:",
-  img: "epicerie.jpg",
-
-  options: [
-    (bananes = {
-      text: "banane",
-      action: "goToChapter('you_die')",
-    }),
-    (pain = {
-      text: "Pain brun",
-      action: "goToChapter('retour_maison')",
-    }),
-  ],
-};
-
-const arivéeÉcole = {
-  subtitle: "Arrivée à l'école",
-  text: "Tu viens d'arriver à l'école, de manière a faire attention a la pandémie tu dois choisir ton occupation judicieusement; tu peux aller dans ta petite classe fermer pour assister a ton cours, tu peux aller vers la cafétéria ou tu pourra travailler sur tes devoirs et avoir plus d'espace ou encore aller dehors...",
-  img: "ecole.jpg",
-
-  options: [
-    (classe = {
-      text: "aller en classe",
-      action: "goToChapter('you_die')",
-    }),
-    (cafet = {
-      text: "Aller a la cafétéria",
-      action: "goToChapter('offre_vaccinale')",
-    }),
-    (dehors = {
-      text: "Pain brun",
-      action: "goToChapter('you_die')",
-    }),
-  ],
-};
-
-const vaccin = {
-  subtitle: "Offre vaccinale",
-  text: "En arrivant à la cafétéria, tu croise un clinique de vaccination qui t'offre un vaccin sur place. Le vaccin semble légérement louche, l'accepte tu malgré ta phobie des aiguilles??",
-  img: "vaccin.jpg",
-
-  options: [
-    (classe = {
-      text: "aller en classe",
-      action: "goToChapter('retour_maison')",
-    }),
-    (cafet = {
-      text: "Aller a la cafétéria",
-      action: "goToChapter('offre_vaccinale')",
-    }),
-    (dehors = {
-      text: "Aller dehors",
-      action: "goToChapter('you_die')",
-    }),
-  ],
-};
-
-const retour = {
-  subtitle: "Retour à la maison",
-  text: "De retour a la maison, tu réalise que tu absolument te rendre à la fête de ton meilleur ami organisée depuis longtemps",
-  img: "fete",
-  /* Si vacciner tu gagne, autrement tu meurt, aucun choix*/
-};
-
-const mort = {
-  subtitle: "VOUS ETES MORT",
-  text: "Tu as atttrapé la maladie, après de long jour à l'hopital à t'accrocher à la vie entouré de ta famille cher, la pandemie a eu raion de toi, repose en paix.",
-  img: "rip.jpg",
-};
 
 const chapterObj = {
-  arivee_ecole: arivéeÉcole,
-  journee_a_maison: journeMaison,
-  aventure_epicerie: choix_bouffe,
-  offre_vaccinale: vaccin,
-  annonce_pandemie: firstchapter,
-  you_die: mort,
-  retour_maison: retour,
+
+  /*Tu te reveille un doux matin de printemps, le soleil illumine doucement ta chambre avec de beaux faissauts lumineux. */
+
+  annonce_pandemie: firstchapter = {
+    subtitle: "La pandémie annoncée",
+    text: "En descandant pour dejeuner tu apprends une grave nouvelle: UNE PANDÉMIE MONDIALE VIENT D'ÊTRE ANNONCÉ TU DOIS TOUT FAIRE EN TON POUVOIR POUR TE PROTEGER TOI ET TA FAMILLE",
+    img: "pandemie.jpg",
+    options: [
+      {
+        text: "Sécher les cours par protection",
+        action: "goToChapter('journee_a_maison')",
+      },
+      {
+        text: "Aller à l'école quand même",
+        action: "goToChapter('arivee_ecole')",
+      },
+    ],
+  },
+
+  journee_a_maison: journeMaison = {
+    subtitle: "Ta journée à la maison",
+    text: "Maintenant que tu ne va plus a l'école, tu dois trouver quelque chose a manger. Tu va regarder dans ton frigo...OH MALHEUR!!! I ne reste plus rine à manger dans ta maison!!!! QUe va tu faire!?",
+    img: "frigo.jpg",
+    options: [
+      {
+        text: "commender de la nourriture",
+        action: "goToChapter('you_die')",
+      },
+      {
+        text: "Aller à l'épicerie",
+        action: "goToChapter('aventure_epicerie')",
+      },
+    ],
+  },
+
+  aventure_epicerie: choix_bouffe = {
+    subtitle: "Aventure à l'épicerie",
+    text: "Tu viens tout juste de renter à l'épicerie, malheureusement du a la pandémie, la majorité des items ont déja été venduent!! De plus tu réalise que tu as oublié ton porte-feuille à la maison.... Tu dois donc te limiter a un items et choisir dans l'inventaire très renstraint:",
+    img: "epicerie.jpg",
+    options: [
+      {
+        text: "banane",
+        action: "goToChapter('you_die')",
+      },
+      {
+        text: "Pain brun",
+        action: "goToChapter('retour_maison')",
+      },
+    ],
+  },
+
+  arivee_ecole: arivéeÉcole = {
+    subtitle: "Arrivée à l'école",
+    text: "Tu viens d'arriver à l'école, de manière a faire attention a la pandémie tu dois choisir ton occupation judicieusement; tu peux aller dans ta petite classe fermer pour assister a ton cours, tu peux aller vers la cafétéria ou tu pourra travailler sur tes devoirs et avoir plus d'espace ou encore aller dehors...",
+    img: "ecole.jpg",
+    options: [
+      {
+        text: "aller en classe",
+        action: "goToChapter('you_die')",
+      },
+      {
+        text: "Aller a la cafétéria",
+        action: "goToChapter('offre_vaccinale')",
+      },
+      {
+        text: "Pain brun",
+        action: "goToChapter('you_die')",
+      },
+    ],
+  },
+
+  offre_vaccinale: vaccin = {
+    subtitle: "Offre vaccinale",
+    text: "En arrivant à la cafétéria, tu croise un clinique de vaccination qui t'offre un vaccin sur place. Le vaccin semble légérement louche, l'accepte tu malgré ta phobie des aiguilles??",
+    img: "vaccin.jpg",
+    options: [
+      {
+        text: "aller en classe",
+        action: "goToChapter('retour_maison')",
+      },
+      {
+        text: "Aller a la cafétéria",
+        action: "goToChapter('offre_vaccinale')",
+      },
+      {
+        text: "Aller dehors",
+        action: "goToChapter('you_die')",
+      },
+    ],
+  },
+  
+  you_die: mort = {
+    subtitle: "VOUS ETES MORT",
+    text: "Tu as atttrapé la maladie, après de long jour à l'hopital à t'accrocher à la vie entouré de ta famille cher, la pandemie a eu raion de toi, repose en paix.",
+    img: "rip.jpg",
+  },
+
+  retour_maison: retour = {
+    subtitle: "Retour à la maison",
+    text: "De retour a la maison, tu réalise que tu absolument te rendre à la fête de ton meilleur ami organisée depuis longtemps",
+    img: "fete",
+    /* Si vacciner tu gagne, autrement tu meurt, aucun choix*/
+  },
 };
 
 function goToChapter(chapterName) {
-  const chapitre = chapterObj.chapterName;
+  const chapitre = chapterObj[chapterName];
   console.log(chapitre.subtitle);
   console.log(chapitre.text);
 }
